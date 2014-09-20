94b85802fe766c0a3465d93c643b1f7a378fd6dd
andrea <mrblasco@gmail.com>
2014-09-20 19:37:54 -0400

    add examples + bug fixes

--- stableRoommate.R
+++ stableRoommate.R
@@ -63,3 +63,38 @@ table(persons) ## check
 outI <- phaseI(persons)
 phaseII(outI)
 
+
+
+## Additional examples from data 
+## 1429
+persons <- as.matrix(read.table('data/example1429.txt')[, -1] + 1)
+ranks <- makeRanks(persons)
+n <- nrow(persons)
+stable1429 <- c(9, 8, 4, 6, 2, 7, 3, 5, 1, 0) + 1
+
+phaseII(phaseI())
+print(stable1429)
+
+## 9774
+persons <- as.matrix(read.table('data/example9774.txt')[, -1] + 1)
+ranks <- makeRanks(persons)
+n <- nrow(persons)
+stable9774 <- c(1, 0, 6, 4, 3, 9, 2, 8, 7, 5)+1
+phaseII(phaseI())
+print(stable9774)
+
+## 3084
+persons <- as.matrix(read.table('data/example3084.txt')[, -1] + 1)
+ranks <- makeRanks(persons)
+n <- nrow(persons)
+stable3084 <- c(7, 6, 5, 9, 8, 2, 1, 0, 4, 3) +1
+phaseII(phaseI())
+print(stable3084)
+
+## 9045
+persons <- as.matrix(read.table('data/example9045.txt')[, -1] + 1)
+ranks <- makeRanks(persons)
+n <- nrow(persons)
+stable9045 <- c(6,2,1,5,9,3,0,8,7,4) + 1
+phaseII(phaseI())
+print(stable9045)
--- stableRoommateFunctions.R
+++ stableRoommateFunctions.R
@@ -8,7 +8,7 @@
 ## Input: NxN matrix of persons id so that (i, j) = id ranked jth by i
 ## Output: NxN matrix of ranks so that (i, j) = rank of person j by i
 makeRanks = function(pers) {
-  if (ncol(pers)>nrow(pers)) {stop('check input matrix dimensions!')}
+  if (ncol(pers)!=nrow(pers)) {stop('check input matrix dimensions!')}
   n <- nrow(pers)
   ranks = matrix(nrow=n, ncol=n) ## init
   for(i in 1:n) {
@@ -18,10 +18,10 @@ makeRanks = function(pers) {
 }
 
 ## FUNCTION to get preference
-## INPUT: (1x1 person id, 1x1 position p, NxN matrix of id, NxN matrix of ranks)
+## INPUT: 1x1 person id, 1x1 position p, NxN matrix of id, NxN matrix of ranks
 ## OUTPUT: list(1x1 new person id ranked in position p by person id, 1x1 position of person id ranked by new person id)
-getPreferences = function(x, i, pers, ranks) {
-  y = pers[x, i] ## i-th ranked person by x
+getPreferences = function(x, i) {
+  y = persons[x, i] ## i-th ranked person by x
   r = ranks[y, x] ## rank of x in i-th person's preference list
   ##return(list(nextPerson = y, nextPersonRank = r))
   return(c(y, r))
@@ -31,59 +31,51 @@ getPreferences = function(x, i, pers, ranks) {
 ## Irving's Phase I of the algorithm
 ## INPUT: NxN matrix of person id
 ## OUTPUT: either false or list()
-phaseI = function(persons) {
-  n <- nrow(persons); ranks = makeRanks(persons);
+phaseI = function() {  
   ## Initialize vars
+  rightperson <- persons[, n]; ## get a proposal from myself
+  leftperson <- persons[, 1]; ## I am proposing to best option 
+  rightrank <- rep(n, n); ## myself is worst
+  leftrank <- rep(1, n); ## 
   holds_proposal <- rep(FALSE, n);
-  rightperson <- persons[, n];  ## the last column
-  rightrank <- rep(n, n); ## worst
-  leftrank <- rep(1, n); ## hasn't been rejected
-  leftperson <- persons[, 1]; ## 
   ## 
   for (i in 1:n) {
     proposer = i;
-    test_until = FALSE;
+    test_until = FALSE; ## Repeat until find (hold proposal == FALSE)
     while ( test_until==FALSE ) {
-      out = getPreferences(proposer, leftrank[proposer], persons, ranks) 
-      while( out[2] > rightrank[out[1]] ) { ## keep searching 
+      out = getPreferences(proposer, leftrank[proposer]) 
+      while( out[2] > rightrank[out[1]] ) {## keep searching 
         leftrank[proposer] <- leftrank[proposer] + 1;
-        out = getPreferences(proposer, leftrank[proposer], persons, ranks)        
+        out = getPreferences(proposer, leftrank[proposer])        
       }
-      previous = rightperson[out[1]] ## previous
+      previous <- rightperson[out[1]] ## previous at the right of
       rightrank[out[1]] <- out[2] ## update rank of receiver
       rightperson[out[1]] <- proposer ## update rightperson of receiver
       leftperson[proposer] <- out[1] ## update left person of proponent
-      proposer = previous ## previous becomes a proponent
+      proposer <- previous ## previous becomes a proponent
       if (holds_proposal[out[1]]==FALSE) {test_until = TRUE;} 
     }
     holds_proposal[out[1]] <- TRUE;
-    if (leftrank[proposer]==n) {
-      return(FALSE);
-    }
+    if (leftrank[proposer]==n) { return(FALSE); }
   }
-  return(list(leftrank = leftrank, leftperson=leftperson, rightrank= rightrank, rightperson = rightperson, holds_proposal=holds_proposal, persons=persons));
+  return(list(leftrank = leftrank, leftperson=leftperson, rightrank=rightrank, rightperson=rightperson));
 }
 
 
 ## Irving's Second phase 
 ## Input from phaseI
 phaseII = function(outPhaseI) {
-  n <- nrow(outPhaseI$persons); persons<-outPhaseI$persons;
-  ## Init Solutions
-  solution_possible = TRUE;
-  solution_found = FALSE;
-  ## Initialize New vars
-  newOut <- list(secondperson = persons[, n], secondrank = rep(n, n), secondrightrank = rep(NA, n) )
+  solution_possible = TRUE; solution_found = FALSE; ## Repeat until 
   while ( solution_possible==TRUE & solution_found==FALSE) {
-    out = seekCycle(outPhaseI, newOut) 
+    out <- seekCycle(outPhaseI)
     if ( !any(!is.na(out$mycycle)) ) { 
       solution_found=TRUE
-    } else {    
-      for (i in out$mycycle[!is.na(out$mycycle)]) { 
+    } else { 
+      for (i in out$mycycle[out$first:out$last]) { 
         outPhaseI$leftrank[i] <- out$secondrank[i];
         outPhaseI$leftperson[i] <- out$secondperson[i];
-        outPhaseI$rightrank[outPhaseI$leftperson[i]] <- out$secondrightrank[i]
-        outPhaseI$rightperson[outPhaseI$leftperson[i]] <- i
+        outPhaseI$rightrank[out$secondperson[i]] <- out$secondrightrank[i]
+        outPhaseI$rightperson[out$secondperson[i]] <- i
       }
       if ( any(outPhaseI$leftrank > outPhaseI$rightrank) ) {solution_possible=FALSE} 
     }
@@ -93,9 +85,12 @@ phaseII = function(outPhaseI) {
 
 
 ### all-or-nothing cycles
-seekCycle = function(outPhaseI, newOut) { 
-  n <- nrow(outPhaseI$persons); ranks <- makeRanks(outPhaseI$persons)
+seekCycle = function(outPhaseI) { 
   ## initialize
+  secondrank <- rep(NA, n);
+  secondperson <- rep(NA, n);
+#  secondrightrank <- rep(NA, n);
+  secondrightrank <- outPhaseI$rightrank
   mycycle = rep(NA, n);
   for (i in 1:n) {
     if (outPhaseI$leftrank[i] < outPhaseI$rightrank[i] ) {break}
@@ -104,26 +99,27 @@ seekCycle = function(outPhaseI, newOut) {
     return( list(mycycle=NA) ) ## return empty cycle
   } else { 
     last = 1;
-    test_until = FALSE
+    test_until = FALSE ## repeat until
     while ( test_until == FALSE ) { 
-      mycycle[last] = i;
-      last = last + 1;
-      p = outPhaseI$leftrank[i];
-      test_until_2 = FALSE;
+      mycycle[last] <- i;
+      last <- last + 1; 
+      p <- outPhaseI$leftrank[i];
+      test_until_2 = FALSE; ## repeat until
       while ( test_until_2 == FALSE ) {
         p = p + 1;
-        out = getPreferences(i, p, persons, ranks);
-        if ( out[2] <= outPhaseI$rightrank[out[1]] ) {test_until_2 = TRUE}
+        out = getPreferences(i, p);
+        if ( out[2] <= secondrightrank[out[1]] ) {test_until_2 = TRUE}
+#        if ( out[2] <= outPhaseI$rightrank[out[1]] ) {test_until_2 = TRUE}
       }
-      newOut$secondrank[i] <- p;
-      newOut$secondperson[i] <- out[1];
-      newOut$secondrightrank[i] <- out[2];
-      i = outPhaseI$rightperson[newOut$secondperson[i]]
+      secondrank[i] <- p;
+      secondperson[i] <- out[1];
+      secondrightrank[i] <- out[2];
+      i <- outPhaseI$rightperson[secondperson[i]] ## a new one is up
       if ( i %in% mycycle ) {test_until = TRUE} 
     }
-    last = last - 1;
-    first = last - 1;
-    while ( mycycle[first] != i ) {first = first -1; }
-    return(list(mycycle=mycycle, secondrank = newOut$secondrank, secondperson=newOut$secondperson, secondrightrank = newOut$secondrightrank));
+    last <- last - 1;
+    first <- last - 1;
+    while ( mycycle[first] != i ) {first <- first - 1 }
+    return(list(first=first, last=last, mycycle=mycycle, secondrank=secondrank, secondperson=secondperson, secondrightrank=secondrightrank));
   }
 }
